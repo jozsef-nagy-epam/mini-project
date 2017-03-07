e42c535b9832254a355b25bdec48732858415976
Jozsef Nagy <jozsef_nagy@epam.com>
2017-03-07 17:23:34 +0100

    properties for repository urls have been added

---
+++ service/src/main/java/com/fortune/cookie/wisdom/service/config/RepositoryData.java
@@ -0,0 +1,45 @@
+package com.fortune.cookie.wisdom.service.config;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriTemplate;
+
+@Component
+@PropertySource(value = "classpath:repositoryURLs.properties")
+public class RepositoryData {
+
+	@Value("${getCategories}")
+	private String categories;
+
+	@Value("${getWisdomsByCategory}")
+	private String wisdomsByCategory;
+
+	@Value("${getWisdomsByCategoryAndId}")
+	private String wisdomsByCategoryAndId;
+
+	public URI getCategoriesURI() {
+		UriTemplate uri = new UriTemplate(categories);
+		return uri.expand();
+	}
+
+	public URI getWisdomsByCategoryURI(String category) {
+		UriTemplate uri = new UriTemplate(wisdomsByCategory);
+		Map<String, String> parameters = new HashMap<>();
+		parameters.put("category", category);
+		return uri.expand(parameters);
+	}
+
+	public URI getWisdomsByCategoryAndIdURI(String category, Long id) {
+		UriTemplate uri = new UriTemplate(wisdomsByCategoryAndId);
+		Map<String, String> parameters = new HashMap<>();
+		parameters.put("category", category);
+		parameters.put("id", id.toString());
+		return uri.expand(parameters);
+	}
+
+}
