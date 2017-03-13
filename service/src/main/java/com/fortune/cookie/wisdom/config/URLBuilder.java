423c35d74e72ed85794792f09b271244971ad459
Jozsef Nagy <jozsef_nagy@epam.com>
2017-03-13 13:08:22 +0100

    base url has been separated and urls have been moved to application.properties

--- service/src/main/java/com/fortune/cookie/wisdom/config/URLBuilder.java
+++ service/src/main/java/com/fortune/cookie/wisdom/config/URLBuilder.java
@@ -5,41 +5,47 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriTemplate;
 
 @Component
-@PropertySource(value = "classpath:repositoryURLs.properties")
 public class URLBuilder {
 
+	@Value("${rootUrl}")
+	private String rootUrl;
 	@Value("${getCategories}")
 	private String categories;
-
 	@Value("${getWisdomsByCategory}")
 	private String wisdomsByCategory;
-
 	@Value("${getWisdomsByCategoryAndId}")
 	private String wisdomsByCategoryAndId;
 
 	public URI getCategoriesURI() {
-		UriTemplate uri = new UriTemplate(categories);
+		UriTemplate uri = new UriTemplate(rootUrl + categories);
 		return uri.expand();
 	}
 
 	public URI getWisdomsByCategoryURI(String category) {
-		UriTemplate uri = new UriTemplate(wisdomsByCategory);
+		UriTemplate uri = new UriTemplate(rootUrl + wisdomsByCategory);
 		Map<String, String> parameters = new HashMap<>();
 		parameters.put("category", category);
 		return uri.expand(parameters);
 	}
 
 	public URI getWisdomByCategoryAndIdURI(String category, Long id) {
-		UriTemplate uri = new UriTemplate(wisdomsByCategoryAndId);
+		UriTemplate uri = new UriTemplate(rootUrl + wisdomsByCategoryAndId);
 		Map<String, String> parameters = new HashMap<>();
 		parameters.put("category", category);
 		parameters.put("id", id.toString());
 		return uri.expand(parameters);
+	}
+
+	String getRootUrl() {
+		return rootUrl;
+	}
+
+	void setRootUrl(String rootUrl) {
+		this.rootUrl = rootUrl;
 	}
 
 	String getCategories() {
