5c49340d96bed1da9912440fabf78f21a3b18d14
Jozsef Nagy <jozsef_nagy@epam.com>
2017-03-13 15:48:43 +0100

    docker-compose.yaml has been added and minor changes have been made in port settings

--- service/src/test/java/com/fortune/cookie/wisdom/service/RestTemplateBasedWisdomSearchServiceTest.java
+++ service/src/test/java/com/fortune/cookie/wisdom/service/RestTemplateBasedWisdomSearchServiceTest.java
@@ -6,42 +6,41 @@ import java.net.URISyntaxException;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import com.fortune.cookie.wisdom.config.URLBuilder;
+import com.fortune.cookie.wisdom.service.domain.CategoryList;
 import com.fortune.cookie.wisdom.service.domain.Wisdom;
+import com.fortune.cookie.wisdom.service.domain.WisdomList;
 
 public class RestTemplateBasedWisdomSearchServiceTest {
-	@InjectMocks
 	private RestTemplateBasedWisdomSearchService underTest;
-	@Mock
-	private URLBuilder repoData;
-	@Mock
+	private URLBuilder urlBuilder;
 	private RestTemplate restTemplate;
 
 	@Before
 	public void setUp() {
-		MockitoAnnotations.initMocks(this);
+		urlBuilder = Mockito.mock(URLBuilder.class);
+		restTemplate = Mockito.mock(RestTemplate.class);
+		underTest = new RestTemplateBasedWisdomSearchService(urlBuilder, restTemplate);
 	}
 
 	@Test
 	public void testGetCategoriesShouldCallRepoApiAndtransformResponse() throws URISyntaxException {
 		// GIVEN
 		URI uri = new URI("");
-		String responseBody = "";
-		ResponseEntity<String> response = new ResponseEntity<>(responseBody, HttpStatus.OK);
-		BDDMockito.given(repoData.getCategoriesURI()).willReturn(uri);
-		BDDMockito.given(restTemplate.getForEntity(uri, String.class)).willReturn(response);
+		CategoryList categories = new CategoryList();
+		ResponseEntity<CategoryList> response = new ResponseEntity<>(categories, HttpStatus.OK);
+		BDDMockito.given(urlBuilder.getCategoriesURI()).willReturn(uri);
+		BDDMockito.given(restTemplate.getForEntity(uri, CategoryList.class)).willReturn(response);
 		// WHEN
 		underTest.getCategories();
 		// THEN
-		BDDMockito.then(repoData).should(BDDMockito.times(1)).getCategoriesURI();
-		BDDMockito.then(restTemplate).should(BDDMockito.times(1)).getForEntity(uri, String.class);
+		BDDMockito.then(urlBuilder).should(BDDMockito.times(1)).getCategoriesURI();
+		BDDMockito.then(restTemplate).should(BDDMockito.times(1)).getForEntity(uri, CategoryList.class);
 	}
 
 	@Test
@@ -49,15 +48,15 @@ public class RestTemplateBasedWisdomSearchServiceTest {
 		// GIVEN
 		String category = "test_category";
 		URI uri = new URI("");
-		String responseBody = "";
-		ResponseEntity<String> response = new ResponseEntity<>(responseBody, HttpStatus.OK);
-		BDDMockito.given(repoData.getWisdomsByCategoryURI(category)).willReturn(uri);
-		BDDMockito.given(restTemplate.getForEntity(uri, String.class)).willReturn(response);
+		WisdomList wisdoms = new WisdomList();
+		ResponseEntity<WisdomList> response = new ResponseEntity<>(wisdoms, HttpStatus.OK);
+		BDDMockito.given(urlBuilder.getWisdomsByCategoryURI(category)).willReturn(uri);
+		BDDMockito.given(restTemplate.getForEntity(uri, WisdomList.class)).willReturn(response);
 		// WHEN
 		underTest.getWisdomsByCategory(category);
 		// THEN
-		BDDMockito.then(repoData).should(BDDMockito.times(1)).getWisdomsByCategoryURI(category);
-		BDDMockito.then(restTemplate).should(BDDMockito.times(1)).getForEntity(uri, String.class);
+		BDDMockito.then(urlBuilder).should(BDDMockito.times(1)).getWisdomsByCategoryURI(category);
+		BDDMockito.then(restTemplate).should(BDDMockito.times(1)).getForEntity(uri, WisdomList.class);
 	}
 
 	@Test
@@ -68,12 +67,12 @@ public class RestTemplateBasedWisdomSearchServiceTest {
 		URI uri = new URI("");
 		Wisdom wisdom = new Wisdom();
 		ResponseEntity<Wisdom> response = new ResponseEntity<>(wisdom, HttpStatus.OK);
-		BDDMockito.given(repoData.getWisdomByCategoryAndIdURI(category, wisdomId)).willReturn(uri);
+		BDDMockito.given(urlBuilder.getWisdomByCategoryAndIdURI(category, wisdomId)).willReturn(uri);
 		BDDMockito.given(restTemplate.getForEntity(uri, Wisdom.class)).willReturn(response);
 		// WHEN
 		underTest.getWisdomByCategoryAndId(category, wisdomId);
 		// THEN
-		BDDMockito.then(repoData).should(BDDMockito.times(1)).getWisdomByCategoryAndIdURI(category, wisdomId);
+		BDDMockito.then(urlBuilder).should(BDDMockito.times(1)).getWisdomByCategoryAndIdURI(category, wisdomId);
 		BDDMockito.then(restTemplate).should(BDDMockito.times(1)).getForEntity(uri, Wisdom.class);
 	}
 }
